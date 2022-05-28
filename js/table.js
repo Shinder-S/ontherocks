c93438fff0f425d59c17c3084fa04bc88578d2ef
Shinder-S <70726598+Shinder-S@users.noreply.github.com>
2022-05-27 23:15:48 -0300

    dynamic table and responsive

--- js/table.js
+++ js/table.js
@@ -1,85 +1,100 @@
 /**** BOTONES ****/
    
-document.querySelector("#btn-add").addEventListener("click", addItems);
+document.querySelector("#btn-add").addEventListener("click", addItem);
 document.querySelector("#btn-add3").addEventListener("click", add3Items);
-document.querySelector("#btn-del").addEventListener("click", delItem);
+document.querySelector("#btn-del").addEventListener("click", delItems);
 
 /**** JSONs ****/
 
-let tableList = [
-        
-                {
-                    name: "Grey Goose",
-                    stockBar: 4,
-                    stockDeposit: 2,
-                    observations: "French D.O.C."
-                },
-                {
-                    name: "Maker's 46",
-                    stockBar: 2,
-                    stockDeposit: 4,
-                    observations: "Best bourbon 2022"
-                },
-                {
-                    name: "Hendrix",
-                    stockBar: 3,
-                    stockDeposit: 3,
-                    observations: "Rock & gin tonic"
-                },
-                {
-                    name: "Tanqueray Sevilla",
-                    stockBar: 2,
-                    stockDeposit: 6,
-                    observations: "Made with orange peel"
-                },
-                {
-                    name: "Habanna",
-                    stockBar: 4,
-                    stockDeposit: 3,
-                    observations: "Special for mojito's"
-                    }                                       
-    ];
-
+let tableList = [    
+            {
+                name: "Grey Goose",
+                stockBar: 4,
+                stockDeposit: 2,
+                observations: "French D.O.C."
+            },
+            {
+                name: "Maker's 46",
+                stockBar: 2,
+                stockDeposit: 4,
+                observations: "Best bourbon 2022"
+            },
+            {
+                name: "Hendrix",
+                stockBar: 1,
+                stockDeposit: 3,
+                observations: "Rock & gin tonic"
+            },
+            {
+                name: "Tanqueray Sevilla",
+                stockBar: 2,
+                stockDeposit: 6,
+                observations: "Made with orange peel"
+            },
+            {
+                name: "Habanna",
+                stockBar: 4,
+                stockDeposit: 3,
+                observations: "Special for mojito's"
+                }                                       
+];
 
 
 function show(){
-let tableDom = document.querySelector("#tableList");
-tableDom.innerHTML= '';
-tableDom.innerHTML +=  `<tr>
-                            <td>Drinks</td>
-                            <td>Stock in bar</td> 
-                            <td>Stock in deposit</td>
-                            <td>Observations</td>                           
-                        </tr>`
+    let tableDom = document.querySelector("#tableList");
+    tableDom.innerHTML= '';
+    let aux = '';
+    tableDom.innerHTML +=  `<tr>
+                                <td>Drinks</td>
+                                <td>Stock in bar</td> 
+                                <td>Stock in deposit</td>
+                                <td>Observations</td>                           
+                            </tr>`
 
-tableList.forEach( stock => {
-    
-});
-   
-let pito = 666
-    tableDom.innerHTML += `<tr ${aux}>
-                                <td>${item.name}</td>
-                                <td>${item.stockBar}</td>
-                                <td>${item.stockDeposit}</td>
-                                <td>${observations}</td>                                                                        
-                            </tr>`;
+    tableList.forEach( stock => {
+        if((stock.stockBar + stock.stockDeposit) <= 4){           
+           tableDom.innerHTML += `<tr class='out-stock'>
+                                    <td>${stock.name}</td>
+                                    <td>${stock.stockBar}</td>
+                                    <td>${stock.stockDeposit}</td>
+                                    <td>${stock.observations}</td>                                                                        
+                                </tr>`;
+        }        
+        tableDom.innerHTML += `<tr>
+                                    <td>${stock.name}</td>
+                                    <td>${stock.stockBar}</td>
+                                    <td>${stock.stockDeposit}</td>
+                                    <td>${stock.observations}</td>                                                                        
+                                </tr>`;
+    }); 
 
-tableDom.innerHTML +=   `<tr>
-                            <td></td>
-                            <td></td>
-                            <td></td> 
-                            <td>TOTAL</td>
-                        </tr>
-                        <tr id="total">
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                            <td >${total}</td>                                 
-                        </tr>`;
+} 
 
-                        
+function addItem(){
+    let name = document.querySelector("#drink-name").value;
+    let stockBar = document.querySelector("#stock-bar").value;
+    let stockDeposit = document.querySelector("#stock-deposit").value;
+    let observations = document.querySelector("#observations").value;
+
+    tableList.push({
+        name: name,
+        stockBar: stockBar,
+        stockDeposit: stockDeposit,
+        observations: observations
+    });
+    show();
+
 }
 
-addBuyItems();
+function add3Items(){
+    for(let index = 1; index <= 3; index++) {
+        addItem();        
+    }
+}
+
+function delItems(){
+    let tableDom = document.querySelector("#tableList");
+    tableDom.innerHTML= '';
+}
 
 show();
